c483dad7ab8f8723c77b609e7aa63cf4bf9da02c
MrFantan <aaronpla@gmail.com>
2022-10-17 10:40:40 +0200

    Update

--- 02_operators.py
+++ 02_operators.py
@@ -1,4 +1,4 @@
-# Operadores #
+# Operadores Aritméticos #
 
 print(3 + 4 )
 print(3 - 4 )
@@ -7,5 +7,32 @@ print(3 / 4 )
 print(10 % 3 )
 print(10 // 3 )
 print(2 ** 3 )
+print(2 ** 3 +3 - 7 /1 // 4)
 
-print("Hola" + "Python")
+print("Hola " + "Python " + "¿Qué tal?")
+print("Hola " + str(5))
+print("Hola " * 5)
+print("Hola " * (2 ** 3)) 
+
+my_float = 2.5 * 2
+print("Hola " * int(my_float))
+
+
+# Operadores Comparativos #
+
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(3 <= 4)
+print(3 == 4)
+print(3 != 4)
+
+print("Hola" > "Python")
+print("Hola" < "Python")
+print("Hola" >= "Zola") # Ordenación Alfabética
+print("Hola" >= "Python")
+print("Hola" <= "Python")
+print("Hola" == "Hola")
+print("Hola" != "Python")
+
+
