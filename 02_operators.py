80b86f876fb1a4401f51914cd2b59cbd1d3f0cf3
MrFantan <aaronpla@gmail.com>
2022-10-17 15:42:21 +0200

    Final Operators

--- 02_operators.py
+++ 02_operators.py
@@ -1,4 +1,4 @@
-# Operadores Aritméticos #
+### Operadores Aritméticos ###
 
 print(3 + 4 )
 print(3 - 4 )
@@ -17,8 +17,7 @@ print("Hola " * (2 ** 3))
 my_float = 2.5 * 2
 print("Hola " * int(my_float))
 
-
-# Operadores Comparativos #
+### Operadores Comparativos ###
 
 print(3 > 4)
 print(3 < 4)
@@ -29,10 +28,16 @@ print(3 != 4)
 
 print("Hola" > "Python")
 print("Hola" < "Python")
-print("Hola" >= "Zola") # Ordenación Alfabética
-print("Hola" >= "Python")
+print("aaaa" >= "abaa") # Ordenación Alfabética por ASCII
+print(len("aaaa") >= len("abaa")) #Cuenta Caracteres
 print("Hola" <= "Python")
 print("Hola" == "Hola")
 print("Hola" != "Python")
 
+### Operadores Lógicos ###
 
+print(3 > 4 and "Hola" > "Python")
+print(3 > 4 or "Hola" > "Python")
+print(3 < 4 or "Hola" < "Python")
+print(3 < 4 or ("Hola" > "Python" and 4 == 4))
+print(not (3 > 4))
