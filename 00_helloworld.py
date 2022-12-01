68abbbc2df91346a3b461b34ce8e5ef8d422585d
MrFantan <aaronpla@gmail.com>
2022-12-01 16:44:37 +0100

    Cambio de nombre

---
+++ 00_helloworld.py
@@ -0,0 +1,31 @@
+### Hola Mundo ###
+
+
+# Esto es un comentario: 
+# Nuestro Hola mundo en python
+print("hola Python") 
+print('hola Python')
+
+# Esto es un comentario
+
+"""
+Este es  
+comentario 
+en varias lineas
+"""
+
+'''
+Este es  
+comentario 
+en varias lineas
+'''
+
+# Consultar el tipo de dato
+print(type("Soy un dato str")) # Tipo 'str'
+print(type(5)) # Tipo 'int'
+print(type(1.5)) # Tipo 'float'
+print(type(1 + 2j)) # Tipo 'complex'
+print(type(True)) # Tipo 'bool'
+print(type(print("Mi cadena de texto"))) # Tipo 'NonType'
+
+
